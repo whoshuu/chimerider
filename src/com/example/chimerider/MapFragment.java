535aa3849ac145ce01b65f44d232260a3de03ca4
Huu Nguyen <huu@prismskylabs.com>
2013-12-06 22:22:34 -0800

    OSM Map works

--- src/com/example/chimerider/MapFragment.java
+++ src/com/example/chimerider/MapFragment.java
@@ -1,6 +1,7 @@
 package com.example.chimerider;
 
 
+import org.osmdroid.DefaultResourceProxyImpl;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.util.ResourceProxyImpl;
@@ -11,20 +12,28 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 public class MapFragment extends Fragment {
 	
 	@Override
-	public View onCreateView(LayoutInflater inf, ViewGroup parent, Bundle savedInstanceState ) {
-		ResourceProxyImpl mResourceProxy = new ResourceProxyImpl(getActivity());
-        MapView mapView = new MapView(getActivity(), 256, mResourceProxy);
-        mapView.setTileSource(TileSourceFactory.MAPQUESTOSM);
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		return inflater.inflate(R.layout.map_fragment, container);
+	}
+	
+	@Override
+	public void onResume() {
+		MapView mapView = (MapView) getActivity().findViewById(R.id.mapview);
         mapView.setClickable(true);
         mapView.setUseDataConnection(false);
+        mapView.setMinZoomLevel(8);
+        mapView.setMaxZoomLevel(15);
         mapView.setBuiltInZoomControls(true);
-        mapView.getController().setZoom(10);
-        mapView.getController().setCenter(new GeoPoint(28.393135, -29.302567));
-        return mapView;
+        mapView.getController().setZoom(12);
+        mapView.getController().setCenter(new GeoPoint(-29.302567, 28.483135));
+        //return mapView;
+        super.onResume();
 	}
 	
 	@Override
